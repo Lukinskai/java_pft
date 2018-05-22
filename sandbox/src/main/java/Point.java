ec28fcc49f2c21ecc9b2c8be93f8cfc6e42d7f5e
Lukinskai <31217624+Lukinskai@users.noreply.github.com>
2018-05-21 20:57:58 -0400

    Point

---
+++ sandbox/src/main/java/Point.java
@@ -0,0 +1,21 @@
+public class Point {
+
+
+    public static void main(String[] args) {
+
+        //public double p1;
+        //public double p2;
+
+        Point p = new Point(7, 9);
+        //System.out.println("Расстояние между двумя точками " + p1 + " и " + p2 + "=" + );
+
+
+        public static double distance (Point p1, Point p2);
+        {
+
+        }
+        public double dist () {
+            return (p1, p2);
+        }
+    }
+}
