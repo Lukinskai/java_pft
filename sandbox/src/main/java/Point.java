30c87f5f5ddf163b1f46286088859bef65d69e53
Lukinskai <31217624+Lukinskai@users.noreply.github.com>
2018-05-29 15:57:00 -0400

    Создание теста addressbook-web-tests и установка депенденсис selenium-java

--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -2,20 +2,23 @@ public class Point {
 
 
     public static void main(String[] args) {
+    } {
 
-        //public double p1;
-        //public double p2;
+    }{
+
+        double a = x2 - x1;
+        double b = y2 - y1;
 
         Point p = new Point(7, 9);
         //System.out.println("Расстояние между двумя точками " + p1 + " и " + p2 + "=" + );
 
 
-        public static double distance (Point p1, Point p2);
+        public static double distance (Point a, Point b);
         {
 
         }
         public double dist () {
-            return (p1, p2);
+            return (p1, p2)
         }
     }
 }
